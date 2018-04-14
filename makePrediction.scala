7d36d1b70af0a8a9f8785436f3b58e2daf7db8b2
werowe <werowe@walkerrowe.com>
2018-04-14 11:48:37 +0100

    Create makePrediction.scala

---
+++ makePrediction.scala
@@ -0,0 +1,44 @@
+import org.apache.spark.mllib.linalg.{Vector, Vectors}
+import org.apache.spark.ml.evaluation.BinaryClassificationEvaluator
+import org.apache.spark.ml.feature.{VectorAssembler, StringIndexer}
+
+var file = "hdfs://localhost:9000/maintenance/maintenance_data.csv"
+
+val sqlContext = new SQLContext(sc)
+val df = sqlContext.read.format("com.databricks.spark.csv").option("header", "true").option("inferSchema", "true").option("delimiter",";").load(file)
+
+val featureCols =  Array("lifetime", "pressureInd", "moistureInd", "temperatureInd")
+
+val assembler = new VectorAssembler().setInputCols(featureCols).setOutputCol("features")
+
+val labelIndexer = new StringIndexer().setInputCol("broken").setOutputCol("label")
+
+val df2 = assembler.transform(df)
+
+val df3 = labelIndexer.fit(df2).transform(df2)
+
+val model = LogisticRegressionModel.load("hdfs://localhost:9000/maintenance/maintenance_model")
+
+val predictions = model.transform(df3)
+
+predictions.select ("team", "provider", "features", "label", "prediction")
+
+var df4 = predictions.select ("team", "provider", "features", "label", "prediction")
+
+val model = LogisticRegression.load("hdfs://localhost:9000/maintenance/maintenance_model2")
+
+val df5 = df4.filter("prediction=1")
+
+df5.show()
+
+val df6 = df5.select("team", "provider")
+
+df6.show()
+
+import java.util.Date
+import java.text.SimpleDateFormat
+val date = new Date()
+var df:SimpleDateFormat = new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss");
+val file = "hdfs://localhost:9000/maintenance/" + df.format(date) + ".csv"
+
+df6.write.format("com.databricks.spark.csv").option("header", "true").save(file)
