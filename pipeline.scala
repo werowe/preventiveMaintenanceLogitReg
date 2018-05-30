dfa330dbf807420e9d23e366f2bf343365fa5104
werowe <werowe@walkerrowe.com>
2018-05-30 10:25:38 +0100

    Update pipeline.scala

--- pipeline.scala
+++ pipeline.scala
@@ -7,48 +7,35 @@ import org.apache.spark.mllib.linalg.{Vector, Vectors}
 import org.apache.spark.ml.evaluation.BinaryClassificationEvaluator
 import org.apache.spark.ml.feature.{VectorAssembler, StringIndexer}
 import org.apache.spark.ml.classification.LogisticRegressionModel
-import java.util.Date
-import java.text.SimpleDateFormat
 import org.apache.spark.ml.{Pipeline, PipelineModel}
+import org.apache.spark.sql.Row
 
 var file = "hdfs://localhost:9000/maintenance/maintenance_data.csv";
 val sqlContext = new SQLContext(sc)
 val df = sqlContext.read.format("com.databricks.spark.csv").option("header", "true").option("inferSchema", "true").option("delimiter",";").load(file)
-df.show()
 
 val featureCols =  Array("lifetime", "pressureInd", "moistureInd", "temperatureInd")
 
 val assembler = new VectorAssembler().setInputCols(featureCols).setOutputCol("features")
 
 val labelIndexer = new StringIndexer().setInputCol("broken").setOutputCol("label")
-
-val df2 = assembler.transform(df)
-
-val df3 = labelIndexer.fit(df2).transform(df2)
 
 val lr = new LogisticRegression()
 
-val pipeline = new Pipeline().setStages(Array(lr))
+val pipeline = new Pipeline().setStages(Array(assembler, labelIndexer, lr))
 
-val model = pipeline.fit(df3)
+
+ val model = pipeline.fit(df)
 
 var predictFile = "hdfs://localhost:9000/maintenance/2018.04.20.15.48.54.csv"
 
-val df = sqlContext.read.format("com.databricks.spark.csv").option("header", "true").option("inferSchema", "true").option("delimiter",";").load(file)
+val testdf = sqlContext.read.format("com.databricks.spark.csv").option("header", "true").option("inferSchema", "true").option("delimiter",";").load(file)
 
-val featureCols =  Array("lifetime", "pressureInd", "moistureInd", "temperatureInd")
+val predictions =  model.transform(testdf)
 
-val assembler = new VectorAssembler().setInputCols(featureCols).setOutputCol("features")
 
-val labelIndexer = new StringIndexer().setInputCol("broken").setOutputCol("label")
+predictions.select("team", "provider", "prediction").filter($"prediction" > 0).collect().foreach { case Row(team: String,provider: String, prediction: Double) =>
+    println(s"($team, $provider, $prediction) --> team=$team, provider=$provider, prediction=$prediction")
+  }
 
-val df2 = assembler.transform(df)
-
-val df3 = labelIndexer.fit(df2).transform(df2)
-
-val predictions = model.transform(df3)
-   
-var df4 = predictions.select ("team", "provider", "pressureInd", "moistureInd", "temperatureInd", "label", "prediction", "probability")
-
-df4.show()
 
